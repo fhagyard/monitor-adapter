ee92e13e09e0442904233a455f457f9e0b730473
BoonMeister <35823370+BoonMeister@users.noreply.github.com>
2018-03-25 19:31:17 +0100

    Update MonitorNetAdapter.ps1
    Add the rest of the script back...

--- MonitorNetAdapter.ps1
+++ MonitorNetAdapter.ps1
@@ -1,3 +1,19 @@
+# Sleep time between each connection test (seconds)
+$SleepTime = 1
+# Number of echo requests to send per test
+$PingCount = 2
+
+# Destination to test (comment out to use default gateway)
+$DestinationHost = "internetbeacon.msedge.net"
+
+# Scriptblock to run on dropout
+$FailureScriptBlock = {
+    Write-Host
+    Write-Host "Connection is down!" -ForegroundColor Red
+    Start-Process notepad.exe -Wait # Do something
+}
+
+# Function - Ping using specific interface
 Function Ping-BySourceIP {
     [CmdletBinding(DefaultParameterSetName="RegularPing")]
     Param(
@@ -84,4 +100,115 @@ Function Ping-BySourceIP {
         }
     }
     Else {Invoke-Expression -Command $MainCommand}
+}
+
+# Prompt to start
+$ConfirmRetry = ""
+$ConfirmStart = ""
+Write-Host "This script will monitor a local interface for any network dropouts"
+While ("YES","Y","NO","N" -notcontains $ConfirmStart) {
+    $ConfirmStart = (Read-Host "Would you like to start?(Y/N)").ToUpper()
+}
+If ("NO","N" -contains $ConfirmStart) {$ConfirmRetry = "N"}
+While ("NO","N" -notcontains $ConfirmRetry) {
+    $ActiveAdapters = Get-WmiObject -Class Win32_NetworkAdapter | Where {$_.NetEnabled -eq $True}
+    If ($ActiveAdapters) {
+        # More than 1 active connection
+        If ($ActiveAdapters.Count -gt 1) {
+            $IndexRange = 1..($ActiveAdapters.Count)
+            Write-Host
+            Write-Warning "More than one network adapter with an active connection was detected."
+            Write-Host
+            Write-Host "Please select the adapter you would like to monitor from the following list:"
+            Write-Host
+            Foreach ($Number in $IndexRange) {
+                $Index = $Number - 1
+                Write-Host "$Number)" $ActiveAdapters[$Index].Name
+            }
+            Write-Host
+            $UserChoice = ""
+            While ($IndexRange -notcontains $UserChoice) {
+                $UserChoice = Read-Host "Please enter the number for your choice"
+            }
+            $Index = $UserChoice - 1
+            $MacAddress = $ActiveAdapters[$Index].MACAddress
+            $InterfaceName = $ActiveAdapters[$Index].Name
+        }
+        # Only 1 active connection
+        Else {
+            $MacAddress = $ActiveAdapters.MACAddress
+            $InterfaceName = $ActiveAdapters.Name
+        }
+        $AdapterConfig = Get-WmiObject -Class Win32_NetworkAdapterConfiguration | Where {$_.MACAddress -eq $MacAddress}
+        If ($AdapterConfig.IPAddress.Count -gt 1) {$SourceIP = $AdapterConfig.IPAddress[0]}
+        Else {$SourceIP = $AdapterConfig.IPAddress}
+        $DefaultGateway = $AdapterConfig.DefaultIPGateway
+        $LastTestOK = $True
+        $OverallTestCounter = 0
+        $DropOutCounter = 0
+        $SuccessfulTestCounter = 0
+        If ($DestinationHost) {$TestDestination = $DestinationHost}
+        Else {$TestDestination = $DefaultGateway}
+        Write-Host
+        Write-Host "Starting monitoring, one moment..."
+        $MonitorStart = Get-Date
+        # Start monitoring loop
+        Do {
+            $ElapsedTime = (Get-Date)-$MonitorStart
+            $TestResult = Ping-BySourceIP -Source $SourceIP -Destination $TestDestination -Count $PingCount -Detailed
+            $OverallTestCounter += 1
+            If ($TestResult.Result -eq $True) {
+                $SuccessfulTestCounter += 1
+                $LastTestOK = $True
+                $LastTestText = "OK"
+                $Colour = "Green"
+            }
+            Else {
+                If ($LastTestOK) {$DropOutCounter += 1}
+                $LastTestOK = $False
+                $LastTestText = "FAILED"
+                $Colour = "Red"
+            }
+            $SuccessRate = ($SuccessfulTestCounter/$OverallTestCounter*100).ToString("0.0") + "%"
+            Clear-Host
+            Write-Host "Start:" $MonitorStart
+            Write-Host "Time elapsed (D:H:M:S): " $ElapsedTime.Days ":" $ElapsedTime.Hours ":" $ElapsedTime.Minutes ":" $ElapsedTime.Seconds -Separator ""
+            Write-Host
+            Write-Host "Interface: $InterfaceName"
+            Write-Host "IP Address: $SourceIP"
+            Write-Host "Default Gateway: $DefaultGateway"
+            Write-Host "Test Destination: $TestDestination"
+            Write-Host
+            Write-Host "Last test result: " -NoNewline
+            Write-Host "$LastTestText" -ForegroundColor $Colour
+            Write-Host "Last test latency avg (ms):" $TestResult.Average
+            Write-Host
+            Write-Host "Total dropouts detected: $DropOutCounter"
+            Write-Host "Total tests: $OverallTestCounter"
+            Write-Host "Successful tests: $SuccessfulTestCounter"
+            Write-Host "Success rate: $SuccessRate"
+            Write-Host
+            Write-Host "Press F5 to stop monitoring"
+            If ($TestResult.Result -eq $False) {Invoke-Command -ScriptBlock $FailureScriptBlock}
+            Start-Sleep -Seconds $SleepTime
+        } While (!($Host.UI.RawUI.KeyAvailable -and ($Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown,IncludeKeyUp").VirtualKeyCode -eq 116)))
+        $MonitorEnd = Get-Date
+        $TotalTime = $MonitorEnd-$MonitorStart
+        Write-Host
+        Write-Host "Script stopped!"
+        Write-Host
+        Write-Host "Start:" $MonitorStart
+        Write-Host "Finish:" $MonitorEnd
+        Write-Host "Total time (D:H:M:S): " $TotalTime.Days ":" $TotalTime.Hours ":" $TotalTime.Minutes ":" $TotalTime.Seconds -Separator ""
+    }
+    # No active connections detected
+    Else {
+        Write-Host
+        Write-Warning "No network adapter with an active Internet connection was detected!`nWhen starting this script please ensure there is at least 1 active network connection"
+    }
+    Write-Host
+    $ConfirmRetry = ""
+    While ("YES","Y","NO","N" -notcontains $ConfirmRetry) {
+        $ConfirmRetry = (Read-Host "Would you like to restart the script?(Y/N)").ToUpper()
+    }
 }
